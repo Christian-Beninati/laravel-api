09bb3510ea14bb2c0dd11c3e633e37e9dc3873dd
Christian-Beninati <christianbeninati.info@gmail.com>
2023-09-12 16:34:13 +0100

    Added Type Projects

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TypeProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 // Resource
 Route::apiResource("projects", ProjectController::class);
+
+
+// TypeProject
+Route::get("types/{type}/projects", [TypeProjectController::class, "index"]);
